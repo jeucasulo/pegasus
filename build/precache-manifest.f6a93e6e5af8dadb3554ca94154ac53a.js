e0e599ced93f0f6e3f424ec747483c42d9d560b5
Jeú Junior <jeucasulo@hotmail.com>
2020-04-27 17:47:22 -0300

    make it better

---
+++ build/precache-manifest.f6a93e6e5af8dadb3554ca94154ac53a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58058b2d2263fcc61ec97faf60fa29ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7798af5b2e2b6c6509a0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75496fb3385148855686",
+    "url": "/static/js/2.ced99e28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ced99e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7798af5b2e2b6c6509a0",
+    "url": "/static/js/main.69119d8b.chunk.js"
+  },
+  {
+    "revision": "0dfe138cef0d715a859c",
+    "url": "/static/js/runtime-main.34b2878b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
